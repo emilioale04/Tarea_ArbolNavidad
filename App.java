2941de26cccb97f2d230a4eea9baa86c1526fa36
emilioale04 <101439067+emilioale04@users.noreply.github.com>
2022-12-20 09:27:36 -0500

    Update App.java

--- App.java
+++ App.java
@@ -1,18 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         char tree[] = "                    ".toCharArray();
-        for (int i = 0; i < (tree.length-2)/2; i++) {
-            tree[(tree.length-2)/2 - i] = tree[i + (tree.length-2)/2] = '+';
+        for (int i = 0; i < tree.length/2; i++) {
+            tree[tree.length/2 - i] = tree[i + tree.length/2] = '+';
             System.out.println(tree);
         }
-        System.out.println("________| |________ :) Emilio");
+        System.out.println("_________| |_________ :) Emilio");
     }
 }
 
 public class App {
     public static void main(String[] args) throws Exception {
         char tree[] = "                    ".toCharArray();
-        for (int i = 1; i < (tree.length)/2-1; i++) {
+        for (int i = 1; i < (tree.length)/2; i++) {
             tree[(tree.length)/2 - i] = tree[i + (tree.length)/2-1] = '+';
             System.out.println(tree);
         }
